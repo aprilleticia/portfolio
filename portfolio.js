1267dfc8e18f967dfc6e65dcaeb00dd69fb55ff1
aprilleticia <april@current.org>
2017-06-21 10:46:47 -0400

    python

---
+++ portfolio.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+// snakephoto1 fades out
+
+$(".snakephoto1").click(function(){
+$(".snakephoto1").fadeOut();
+
+// snakephoto2 fades out
+
+$(".snakephoto2").click(function(){
+$(".snakephoto2").fadeOut();
+
+});
+
+// snakephoto3 fades out
+
+$(".snakephoto3").click(function(){
+$(".snakephoto3").fadeOut();
+
+});
+
+// snakephoto4 fades out
+
+
+$(".snakephoto4").click(function(){
+$(".snakephoto4").fadeOut();
+
+});
+
+
+
+    });
+});
